2c0db13748896017d366cd7a10cb7e0e1d2e95d2
quesera2 <quesera2262@gmail.com>
2026-03-20 10:43:37 +0900

    GitHubApiをリファクタリング

--- data/api/src/commonMain/kotlin/que/sera/sera/githubbrowser2/GitHubApi.kt
+++ data/api/src/commonMain/kotlin/que/sera/sera/githubbrowser2/GitHubApi.kt
@@ -3,17 +3,36 @@ package que.sera.sera.githubbrowser2
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.request.get
+import io.ktor.http.URLProtocol
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
-class GitHubApi {
+class GitHubApi(
+    json: Json
+) {
+
     private val client = HttpClient {
         install(ContentNegotiation) {
-            json(Json { ignoreUnknownKeys = true })
+            json(json)
+        }
+        defaultRequest {
+            url {
+                protocol = URLProtocol.HTTPS
+                host = "api.github.com"
+            }
         }
     }
 
-    suspend fun fetchRepos(username: String): List<GitHubRepo> =
-        client.get("https://api.github.com/users/$username/repos?per_page=100&sort=updated").body()
+    suspend fun fetchRepos(
+        username: String,
+        perPage: Int = 100,
+        sort: String = "updated"
+    ): List<GitHubRepo> = client.get("users/$username/repos") {
+        url {
+            parameters.append("per_page", perPage.toString())
+            parameters.append("sort", sort)
+        }
+    }.body()
 }
--- shared/src/commonMain/kotlin/que/sera/sera/githubbrowser2/DataModule.kt
+++ shared/src/commonMain/kotlin/que/sera/sera/githubbrowser2/DataModule.kt
@@ -4,10 +4,15 @@ import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.ContributesTo
 import dev.zacsweers.metro.Provides
 import dev.zacsweers.metro.SingleIn
+import kotlinx.serialization.json.Json
 
 @ContributesTo(AppScope::class)
 interface DataModule {
     @Provides
     @SingleIn(AppScope::class)
-    fun provideGitHubApi(): GitHubApi = GitHubApi()
+    fun provideJson(): Json = Json { ignoreUnknownKeys = true }
+
+    @Provides
+    @SingleIn(AppScope::class)
+    fun provideGitHubApi(json: Json): GitHubApi = GitHubApi(json)
 }
